ea2f1fe351c9ab48c640963c20da13b91df50171
LePingouinViking <96548849+LePingouinViking@users.noreply.github.com>
2025-12-01 05:34:02 +0100

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# name: discourse-fiche-perso-json
+# about: Affiche une fiche perso formatée à partir des pièces jointes JSON
+# version: 0.1
+# authors: LePingouin & Noa
+# url: https://github.com/LePingouin/discourse-fiche-perso-json
+
+register_asset "stylesheets/common/fiche-perso-json.scss"
